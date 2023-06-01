21eb63bc100c8900ffb43cc6f7cf13489f443b7f
lrandolph23 <lacy.randolph23@gmail.com>
2023-05-31 21:41:14 -0500

    Almostv2

--- components/views/Weather.js
+++ components/views/Weather.js
@@ -5,17 +5,16 @@ export default (state) => html`
 <div class="left">
 <fieldset>
 <legend>Search by Date</legend>
-<label for="date">Date:
 <form id="yes" method="POST" action="">
 <input id="date" type="date" name="date">
 <input type="submit" value="Submit">
 </form>
-</label>
 </fieldset>
 </div>
 <div class="right">
-<div class=info>For date: ${state.start}</div>
-<div class="info">The closest object near ${state.obj.orbiting} is asteroid "${state.obj.name}" </div>
+<div class="info">Date: ${state.start}</div>
+<div class="info">Flying Object: ${state.obj.name} </div>
+<div class="info">Orbiting Body: ${state.obj.orbiting}</div>
 <div class="info">Distance from orbiting body: ${state.obj.miles} miles.</div>
 <div class="info">Size: ${state.obj.size} feet in diameter.</div>
 <div class="info">Hazardous: ${state.obj.hazard}.</div>
